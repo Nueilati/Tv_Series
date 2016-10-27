bea7c3ef5196c7641851ee468c59e1de964b455a
Nueilati <nueilati.s@gmail.com>
2016-10-27 10:58:06 +0200

    Update favorite.php

--- favorite.php
+++ favorite.php
@@ -6,7 +6,7 @@
 				<center> <img src="img/pic1.jpg" height="360px" width="540px"> </center>
 				<blockquote><p> All Due Respect.
 				<br><small> — Tony Soprano </small></p></blockquote>
-				<p>I have recently finished watching all the six seasons of The Sopranos. And I can finally understand why this is “one of the best – if not the best television series ever created”. </p>
+				<p> I have recently finished watching all the six seasons of The Sopranos. And I can finally understand why this is “one of the best – if not the best television series ever created”. </p>
 				<p> We can see the beauty of New Jersay all over the show with James Gandolfini playing Tony Soprano aka a waste management consultant officialy and unofficialy being the Boss of the New Jersay mob crew. </p>
 				<p> This show isn't only about mafiosi, crime, murder or drugs. This is mainly a dissection about the notion of family and family values. Indeed Tony Soprano has two families in the show : one is own family and the other one is mob crew. </p>
 				<p> Anthony Soprano is vulgar, uneducated, sexist, violent, proud, an unfaithful believer, racist, immoral, ... In short, Anthony Soprano is American. </p>
